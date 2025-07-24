97fc87c0d6e10e110e0d63efa27e34df7724b7eb
moderncavemann <130747437+moderncavemann@users.noreply.github.com>
2025-07-24 14:16:52 +1000

    Update fuse_scores.py

--- modules/fuse_scores.py
+++ modules/fuse_scores.py
@@ -1,8 +1,8 @@
-def fuse_scores(score_text, score_image, score_audio,
-                w_text=0.5, w_image=0.3, w_audio=0.2):
+def fuse_scores(score_text, score_image, 
+                w_text=0.5, w_image=0.5, ):
     final_score = (w_text * score_text +
                    w_image * score_image +
-                   w_audio * score_audio)
+                  )
 
     if final_score > 0.3:
         return "BUY ✅", final_score
